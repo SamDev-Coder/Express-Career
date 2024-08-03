e79137bd8456f1e3123c6a27cd43d983e5555d6a
SamDev-Coder <126113961+SamDev-Coder@users.noreply.github.com>
2024-08-03 08:04:29 +0000

    Made changes to server mustache

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@
  const path = require('path');
 
  const JOBS = require('./jobs');
-const mustacheExpress = require('mustache-express');
+// const mustacheExpress = require('mustache-express');
 
 
  const app = express();
 
- app.use(express.static(path.join(__dirname, 'public')));
- app.set('views', path.join(__dirname, 'pages'));
- app.set('view engine', 'mustache');
- app.engine('mustache', mustacheExpress());
+//  app.use(express.static(path.join(__dirname, 'public')));
+//  app.set('views', path.join(__dirname, 'pages'));
+//  app.set('view engine', 'mustache');
+//  app.engine('mustache', mustacheExpress());
 
  app.get('/', (reg, res)=>{
     // res.send('Hello Spellz!');
-   //  res.sendFile(path.join(__dirname, 'pages/index.html'));
-   res.render('index', {jobs: JOBS});
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+   // res.render('index', {jobs: JOBS});
  });
 
  const port = process.env.PORT || 3000;
