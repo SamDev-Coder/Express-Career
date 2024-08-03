6166bf6c8e617dd9edd8b5f3e4cbfa8f7b26d52b
SamDev-Coder <126113961+SamDev-Coder@users.noreply.github.com>
2024-08-03 07:22:01 +0000

    added index.mustache

---
+++ src/jobs.js
@@ -0,0 +1,27 @@
+const JOBS = [
+    {
+        id: 1, 
+        title: 'Frontend Development',
+        location: 'Benge, Lagos',
+        salary: '120000',
+        posted: 'Match 3, 2023'
+    },
+    {
+        id: 2,
+        title: 'Special Development',
+        location: 'Benge, Lagos',
+        salary: '120000',
+        posted: 'Match 3, 2023'
+    }, 
+    
+    {
+        id: 3,
+        title: 'Full Development',
+        location: 'Benge, Lagos',
+        salary: '120000',
+        posted: 'Match 3, 2023'
+    }, 
+    
+]
+
+module.exports = JOBS;
